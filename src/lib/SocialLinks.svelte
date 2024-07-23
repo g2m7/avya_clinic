8ad0f939c802c60299ae0735cfdfe2321d92cee9
Kinnor Majumder <kinnormajumder.fiverr@gmail.com>
2024-07-23 20:58:48 +0530

    add social

--- src/lib/SocialLinks.svelte
+++ src/lib/SocialLinks.svelte
@@ -2,6 +2,7 @@
   <div class="container mx-auto">
     <div class="text-center">
       <h3 class="text-2xl text-white font-radley ">Follow @dr_avirup on Instagram</h3>
+      <h3 class="text-2xl text-white font-radley ">Follow @dr_avirup on Facebook</h3>
       <!-- You can add Instagram feed or social media icons here -->
     </div>
   </div>
